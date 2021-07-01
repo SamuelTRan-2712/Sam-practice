25f3179ce1fddf51ecab6438e26c451ea8e52c51
ladykiller374 <baingheielts@gmail.com>
2021-07-01 16:34:31 -0400

    practicing

---
+++ easy_gui_testing.py
@@ -0,0 +1,42 @@
+from tkinter import *
+import tkinter
+
+def sams_calculator():
+    number1 = Entry.get(E1)
+    number2 = Entry.get(E2)
+    operator = Entry.get(E3)
+    number1 = int(number1)
+    number2 = int(number2)
+
+    if operator == "+":
+        result = number1 + number2
+    if operator == "-":
+        result = number1 - number2
+    if operator == "/":
+        result = number1 / number2
+    if operator == "*":
+        result = number1 * number2
+    Entry.insert(E4,0, result)
+    print(result)
+
+
+top = tkinter.Tk()
+
+L1 = Label(top, text="Sam's small calculator").grid(row=0, column=1)
+L2 = Label(top, text="number 1").grid(row=1, column=0)
+L3 = Label(top, text="number 2").grid(row=2, column=0)
+L4 = Label(top, text="operator").grid(row=3, column=0)
+L5 = Label(top, text="result").grid(row=4, column=0)
+
+E1 = Entry(top, bd=5)
+E1.grid(row=1, column=1)
+E2 = Entry(top, bd=5)
+E2.grid(row=2, column=1)
+E3 = Entry(top, bd=5)
+E3.grid(row=3, column=1)
+E4 = Entry(top, bd=5)
+E4.grid(row=4, column=1)
+
+B = Button(top, text="get result", command=sams_calculator).grid(rows=5, column=1)
+
+top.mainloop()
