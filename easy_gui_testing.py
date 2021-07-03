7e3d1c861710cebe0c94afebe549a7f1c05bdd54
ladykiller374 <baingheielts@gmail.com>
2021-07-03 15:33:31 -0400

    practicing

--- easy_gui_testing.py
+++ easy_gui_testing.py
@@ -1,31 +1,37 @@
 from tkinter import *
 import tkinter
+from tkinter import messagebox
+
 
 def sams_calculator():
-    number1 = Entry.get(E1)
-    number2 = Entry.get(E2)
-    operator = Entry.get(E3)
-    number1 = int(number1)
-    number2 = int(number2)
+    try:
+        number1 = Entry.get(E1)
+        number2 = Entry.get(E2)
+        operator = Entry.get(E3)
+        number1 = int(number1)
+        number2 = int(number2)
 
-    if operator == "+":
-        result = number1 + number2
-    if operator == "-":
-        result = number1 - number2
-    if operator == "/":
-        result = number1 / number2
-    if operator == "*":
-        result = number1 * number2
-    Entry.insert(E4,0, result)
-    print(result)
+        if operator == "+":
+            result = number1 + number2
+        if operator == "-":
+            result = number1 - number2
+        if operator == "/":
+            result = number1 / number2
+        if operator == "*":
+            result = number1 * number2
+        Entry.insert(E4, 0, result)
+        print(result)
+    except ValueError:
+        messagebox.showinfo("Error warning", "please enter integers only")
 
 
 top = tkinter.Tk()
 
-L1 = Label(top, text="Sam's small calculator").grid(row=0, column=1)
-L2 = Label(top, text="number 1").grid(row=1, column=0)
-L3 = Label(top, text="number 2").grid(row=2, column=0)
-L4 = Label(top, text="operator").grid(row=3, column=0)
+
+L1 = Label(top, text="Sam's small calculator", foreground="red").grid(row=0, column=1)
+L2 = Label(top, text="number 1", background="yellow", foreground="blue").grid(row=1, column=0)
+L3 = Label(top, text="number 2", background="yellow", foreground="blue").grid(row=2, column=0)
+L4 = Label(top, text="operator", background="green", foreground="white").grid(row=3, column=0)
 L5 = Label(top, text="result").grid(row=4, column=0)
 
 E1 = Entry(top, bd=5)
